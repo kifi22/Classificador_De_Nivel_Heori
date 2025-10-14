795c7fa98cc4108e722e4df7f7131c6accd42ca1
kifi22 <wicliffydias2@gmail.com>
2025-10-14 19:14:41 -0300

    Projeto Classificador de Heroi

---
+++ index.js
@@ -0,0 +1,106 @@
+let nome_heroi = "Pedro";
+let Xp = 100;
+
+
+let Ferro = (Xp < 1000)
+let ranking_1 = "Ferro"
+
+    if (Xp > 100 && Xp < 1000)
+{
+    switch (Ferro){
+        case Ferro:
+        console.log ("O heroi de nome " + nome_heroi + " está no nivel " + ranking_1)
+        break
+    }
+
+}
+
+let Bronze = (Xp > 1000 && Xp < 2001)
+let ranking_2 = "Bronze"
+
+    if (Xp > 1000 && Xp < 2001)
+{
+    switch (Bronze){
+        case Bronze:
+        console.log ("O heroi de nome " + nome_heroi + " está no nivel " + ranking_2)
+        break
+    }
+
+}
+
+let Prata = (Xp > 2000 && Xp < 5001)
+let ranking_3 = "Prata"
+
+    if (Xp > 2000 && Xp < 5001)
+{
+    switch (Prata){
+        case Prata:
+        console.log ("O heroi de nome " + nome_heroi + " está no nivel " + ranking_3)
+        break
+    }
+}
+
+let Ouro = (Xp > 5000 && Xp < 7001)
+let ranking_4 = "Ouro"
+
+    if (Xp > 5000 && Xp < 7001)
+{
+    switch (Ouro){
+        case Ouro:
+        console.log ("O heroi de nome " + nome_heroi + " está no nivel " + ranking_4)
+        break
+    }
+}
+
+let Platina = (Xp > 7000 && Xp < 8001)
+let ranking_5 = "Platina"
+
+    if (Xp > 7000 && Xp < 8001)
+{
+    switch (Platina){
+        case Platina:
+        console.log ("O heroi de nome " + nome_heroi + " está no nivel " + ranking_5)
+        break
+    }
+}
+
+let Ascendente = (Xp > 8000 && Xp < 9001)
+let ranking_6 = "Ascendente"
+
+    if (Xp > 8000 && Xp < 9001)
+{
+    switch (Ascendente){
+        case Ascendente:
+        console.log ("O heroi de nome " + nome_heroi + " está no nivel " + ranking_6)
+        break
+    }
+}
+
+let Imortal = (Xp > 9000 && Xp < 10001)
+let ranking_7 = "Imortal"
+
+    if (Xp > 9000 && Xp < 10001)
+{
+    switch (Imortal){
+        case Imortal:
+        console.log ("O heroi de nome " + nome_heroi + " está no nivel " + ranking_7)
+        break
+    }
+}
+
+let Radiante = (Xp >= 10001)
+let ranking_8 = "Radiante"
+
+    if (Xp >= 10001)
+{
+    switch (Radiante){
+        case Radiante:
+        console.log("O heroi de nome " + nome_heroi + " está no nivel " + ranking_8)
+        break
+    }
+}
+
+while(Xp < 100){
+    console.log ("Jogador iniciante detectado")
+    break
+}
